5fe308725ba159ef424870d4542402f804c8b90d
iustin948 <nicolaie-iustin.homoranu@student.tuiasi.ro>
2024-01-12 13:35:51 +0200

    header work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [			
@@ -16,7 +17,8 @@ import { ProductPageComponent } from './product-page/product-page.component';
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
