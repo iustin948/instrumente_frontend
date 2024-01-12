9ee49570453e3f9fcbe9cce12eddd4bd389609a5
iustin948 <nicolaie-iustin.homoranu@student.tuiasi.ro>
2024-01-12 11:22:10 +0200

    before update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      HomePageComponent,
+      HeaderComponent,
+      ProductPageComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
