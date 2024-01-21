d04b31c03a00a2f1860316b680e13d4b8e5b116e
iustin948 <homoranuiustin@gmail.com>
2024-01-21 20:30:32 +0200

    main page work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,25 @@ import { HeaderComponent } from './header/header.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { CardComponent } from "./card/card.component";
+
 
 @NgModule({
-  declarations: [			
-    AppComponent,
-      HomePageComponent,
-      HeaderComponent,
-      ProductPageComponent
-   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ButtonModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomePageComponent,
+        HeaderComponent,
+        ProductPageComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ButtonModule,
+        CardModule,
+        CardComponent
+    ]
 })
 export class AppModule { }
